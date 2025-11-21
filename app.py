23f3d29141b3724832c8af5f30124eb4016e5f96
kingnathan-15 <74835239+kingnathan-15@users.noreply.github.com>
2025-11-21 12:19:53 +0300

    Comment out app execution block
    Comment out the main execution block for the app.

--- app.py
+++ app.py
@@ -69,5 +69,5 @@ def submit():
     return render_template('next_user.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
